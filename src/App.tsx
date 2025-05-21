4b8089612905788118f3beb94c2cdedbe03868a8
fishrifle <163429092+fishrifle@users.noreply.github.com>
2025-05-21 12:35:51 -0500

    feat: removed fold ui and button pararaph from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import Button from "./ui/Button";
-import Paragraph from "./ui/Paragraph";
+
 export function App() {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <Button size="md" variant="primary">
-          Shop Collection
-        </Button>
-        <div className="">
-          <Paragraph as="p" variant="body" className="max-w-xl">
-            Discover our new collection of premium shoes designed for both
-            comfort and style. Crafted with the finest materials and attention
-            to detail.
-          </Paragraph>
+<div>  
         </div>
-      </div>
-    </>
+      
+  
   );
 }
 export default App;
